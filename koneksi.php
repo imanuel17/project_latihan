aaa3c4b986465c37ac7188dc53e73d6255274610
imanuel17 <lutherluther31@gmail.com>
2018-02-13 10:16:41 +0700

    koneksi untuk database

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+<?php 
+$dbtype = "mysql"; // ini tipe databasenya
+	$dbhost = "localhost"; //host server
+	$dbname = "sampledb";		//nama database
+	$dbuser = "root";	//username database
+	$dbpassword = "";		//password database
+	$dbcharset = "utf8";		//character database
+	
+	$dsn = "mysql:host=$dbhost;dbname=$dbname;charset=$dbcharset";	
+
+	$dbconn = new PDO($dsn, $dbuser, $dbpassword);	
+	?>
